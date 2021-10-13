f1f0253bc486afce6cbfcc4909d496891aa24222
Jorgen-son-of-karl <75936756+Jorgen-son-of-karl@users.noreply.github.com>
2021-10-13 15:18:08 +0200

    sås

--- main.js
+++ main.js
@@ -1,91 +1,94 @@
-const form = document.querySelector('form');
-
-const todoList = document.getElementById('todoList');
-
-const todo = [];
-
+let totalLines = 0;
+var allBtn = document.getElementById("all");
+var activeBtn = document.getElementById("active");
+var completeBtn = document.getElementById("completed");
 const categoryBtns = document.querySelectorAll('.categoryBtn');
+const markAllBtn = document.getElementById("markAll");
+var container = document.getElementById("container");
+var form = document.querySelector("form");
+let allChecked = false;
 
-var allTodos = [];
-
-var completedTodos = [];
-
-var activeTodos = [];
-
-document.onkeydown = function (event, keycode) {
-    if (event.keyCode === 13) {
-    	if(form.elements.todoInput.value != "") {
-	        event.preventDefault();
-			removeAllChildren(todoList);
-			userInput = form.elements.todoInput.value;
-			todo.push(userInput);
-			todo.forEach(printList);
-			allTodos.push(userInput);
-			form.reset();
-    	}
-    	else{
-    		event.preventDefault();
-    	}
-    }
+completeBtn.onclick = function(){
+	filter("completed");
 };
 
-function removeAllChildren(parent){
-	while(parent.firstChild){
-		parent.removeChild(parent.firstChild);
-	}
+activeBtn.onclick = function(){
+	filter("active");
+};
+
+allBtn.onclick = function(){
+	filter("all");
+};
+
+function filter(method) {
+
+    var checkboxes = document.getElementsByClassName("check");
+
+    for (var i = 0; i < checkboxes.length; i++) {
+
+        // Vi sparar om checkboxen för denna iteration är true/false för att korta ned if-statement nedanför
+        var checked = checkboxes.item(i).checked;
+
+        if((checked && method == 'completed') || (!checked && method == 'active') || method == 'all') {
+            checkboxes.item(i).parentNode.style.display = 'list-item';
+        }
+        else {
+            checkboxes.item(i).parentNode.style.display = 'none';
+        }
+    }
 }
 
-function printList(item){
+function check(checked = true) {
+    const cbs = document.querySelectorAll('.check');
+    cbs.forEach((cb) => {
+    	if(allChecked == false){
+    		cb.checked = true;
+    	}
+    	else{
+    		cb.checked = false;
+    	}
+        
+    });
+    allChecked = allChecked ? false : true;
+}
 
-// OBS FIXA DETTA
-//arrayen blir inte 0, den blir ingenting
+markAllBtn.onclick = function(){
+	check();
+}
 
-	//visar knappar när todolist innehåller någonting
-	for (var i = 0; i < categoryBtns.length; i++){
+document.onkeydown = function (event, keycode) {
+    if (event.keyCode === 13){
+    	if(form.elements.todoInput.value != "") {
+			AddNewLine();
+			form.reset();
+    	}
+		event.preventDefault();
+    }
+}
+
+function AddNewLine() {
+  totalLines++;
+  
+  	for (var i = 0; i < categoryBtns.length; i++){
 		categoryBtns[i].style.display = "inline-block";
 	}
-	// create div
-	var row = document.createElement('div');
-	row.className = 'lineDiv';
-
-	// create checkbox
-	var completeBox = document.createElement('input');
-	completeBox.type = 'checkbox';
-
-	// create listItem and text
-	var listItem = document.createElement('li');
-	var todoEntry = document.createTextNode(item);
-
-	// create delete button
-	var deleteBtn = document.createElement('button');
-	deleteBtn.textContent = '❌';
-	deleteBtn.className ='delete'
-
-
-	listItem.appendChild(completeBox); // listItem > checkbox
-	listItem.appendChild(todoEntry); // listItem > textNode
-	listItem.appendChild(deleteBtn); // listItem > deleteBtn
-	row.appendChild(listItem); // div > listItem
-	todoList.appendChild(row); // todoList > div
-
-	//we give the delete button the id of the item index we use in our parameter
-	deleteBtn.id = todo.indexOf(item);
-	//completeBox.id = todo.indexOf(item + "a");
-	deleteBtn.addEventListener("click",  function(){
-
-		removeAllChildren(todoList);
-		//we grab the parent element of the button with the selected id
-		todo.splice(event.srcElement.id, 1);
-		todo.forEach(printList);
-		//kontrollerar todo om den är tom, om den är det så ska knappar döljas
-			var styling = (todo.length > 0) ? "inline-block" : "none";
-			for (var i = 0; i < categoryBtns.length; i++){
-				categoryBtns[i].style.display = styling;
-			}
-			console.log(todo);
-		form.reset();
-	});		
+  
+  var textInput = document.getElementById("todoInput").value;
+  var todoList = document.getElementById("todoList");
+  var newLine = document.createElement("li");
+  newLine.innerHTML = "<input class='check' type='checkbox' id='"+totalLines+"'> "+textInput+" <span class=" +"delete"+ " onclick='delLine("+totalLines+");'>❌</span>";
+  
+  container.insertBefore(newLine, todoList);
+  
 }
 
+function delLine(lineID) {
+document.getElementById(lineID).parentNode.remove();
+totalLines--;
 
-
+	if(totalLines<1) {
+		for (var i = 0; i < categoryBtns.length; i++){
+			categoryBtns[i].style.display = "none";
+		}
+	}
+}
