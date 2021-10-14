d29cabc2ae0dd8f6b93a01a5ef1a7febf46bdbbb
Jorgen-son-of-karl <75936756+Jorgen-son-of-karl@users.noreply.github.com>
2021-10-14 17:43:23 +0200

    kommentarer skrivna

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//here we declare some global variables
 let totalLines = 0;
 var allBtn = document.getElementById("all");
 var activeBtn = document.getElementById("active");
@@ -8,6 +9,8 @@ var container = document.getElementById("container");
 var form = document.querySelector("form");
 let allChecked = false;
 
+// the buttons to show completed/active/all todos does similar things so we just
+// call the filter method with different parameters
 completeBtn.onclick = function(){
 	filter("completed");
 };
@@ -20,15 +23,11 @@ allBtn.onclick = function(){
 	filter("all");
 };
 
+//this is our method that filters what to show, depending on whats checked as completed, and what button we pressed
 function filter(method) {
-
     var checkboxes = document.getElementsByClassName("check");
-
     for (var i = 0; i < checkboxes.length; i++) {
-
-        // Vi sparar om checkboxen för denna iteration är true/false för att korta ned if-statement nedanför
         var checked = checkboxes.item(i).checked;
-
         if((checked && method == 'completed') || (!checked && method == 'active') || method == 'all') {
             checkboxes.item(i).parentNode.style.display = 'list-item';
         }
@@ -38,6 +37,8 @@ function filter(method) {
     }
 }
 
+//this funcion changes all checboxes to checked, unless all is allready checked, then it unchecks them
+//effectively, a toggle function
 function check(checked = true) {
     const cbs = document.querySelectorAll('.check');
     cbs.forEach((cb) => {
@@ -51,10 +52,15 @@ function check(checked = true) {
     allChecked = allChecked ? false : true;
 }
 
+//we call our "toggle" function on this button
 markAllBtn.onclick = function(){
 	check();
 }
 
+//this eventhandler starts if we press "enter"
+//first we check if the user has written anything in the input
+//then we add the new todo, reset the form so we get back our placeholder text
+//and we prevent the default behaviour so the page doesnt get refreshed
 document.onkeydown = function (event, keycode) {
     if (event.keyCode === 13){
     	if(form.elements.todoInput.value != "") {
@@ -65,26 +71,32 @@ document.onkeydown = function (event, keycode) {
     }
 }
 
+//this function adds the user input on our todolist
 function AddNewLine() {
-  totalLines++;
-  
+	//this variable keeps track on how many lines we have
+  totalLines++; 
+  //if we call this function we want our category buttons to be shown
   	for (var i = 0; i < categoryBtns.length; i++){
 		categoryBtns[i].style.display = "inline-block";
 	}
-  
   var textInput = document.getElementById("todoInput").value;
   var todoList = document.getElementById("todoList");
   var newLine = document.createElement("li");
-  newLine.innerHTML = "<input class='check' type='checkbox' id='"+totalLines+"'><div class=note>"+textInput+"</div> <span class=" +"delete"+ " onclick='delLine("+totalLines+");'>❌</span>";
+  //here we add to our li a checkbox and a delete span with the id of the variable that keeps tracks on our lines
+  newLine.innerHTML = "<input class='check' type='checkbox' id='"+totalLines+"'>" +textInput+" <span class=" +"delete"+ " onclick='delLine("+totalLines+");'>❌</span>";
   
   container.insertBefore(newLine, todoList);
   
 }
 
+//this delete function gets called when we click the del span
 function delLine(lineID) {
+	//it removes the parent of the element with the lineID
 document.getElementById(lineID).parentNode.remove();
+//the variable gets updated
 totalLines--;
 
+//and if we dont have any todos we want to hide theese buttons again
 	if(totalLines<1) {
 		for (var i = 0; i < categoryBtns.length; i++){
 			categoryBtns[i].style.display = "none";
