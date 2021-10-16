2013d4ab2fdb162ef70d3c1a89f6f5826f991303
Jorgen-son-of-karl <75936756+Jorgen-son-of-karl@users.noreply.github.com>
2021-10-16 09:17:11 +0200

    -

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let allChecked = false;
 var removeCompleted = document.getElementById("removeCompleted");
 var checkedItems = 0;
 
+
 // the buttons to show completed/active/all todos does similar things so we just
 // call the filter method with different parameters
 completeBtn.onclick = function(){
@@ -27,7 +28,7 @@ allBtn.onclick = function(){
 
 //this is our method that filters what to show, depending on whats checked as completed, and what button we pressed
 function filter(method) {
-    var checkboxes = document.getElementsByClassName("check");
+    //var checkboxes = document.getElementsByClassName("check");
     for (var i = 0; i < checkboxes.length; i++) {
         var checked = checkboxes.item(i).checked;
         if((checked && method == 'completed') || (!checked && method == 'active') || method == 'all') {
@@ -53,7 +54,7 @@ function check(checked = true) {
     });
     allChecked = allChecked ? false : true;
 }
-console.log(checkedItems);
+
 
 //we call our "toggle" function on this button
 markAllBtn.onclick = function(){
@@ -77,22 +78,22 @@ document.onkeydown = function (event, keycode) {
 //this function adds the user input on our todolist
 function AddNewLine() {
 	//this variable keeps track on how many lines we have
-  totalLines++; 
-  //if we call this function we want our category buttons to be shown
-  	for (var i = 0; i < categoryBtns.length; i++){
-		categoryBtns[i].style.display = "inline-block";
-	}
-  var textInput = document.getElementById("todoInput").value;
-  var todoList = document.getElementById("todoList");
-  var newLine = document.createElement("li");
-  //here we add to our li a checkbox and a delete span with the id of the variable that keeps tracks on our lines
-  newLine.innerHTML = "<input class='check' type='checkbox' id='"+totalLines+"'><div class='note'>"+textInput+"</div> <span class=" +"delete"+ " onclick='delLine("+totalLines+");'>❌</span>";
+	totalLines++; 
+	    //if we call this function we want our category buttons to be shown
+	  	for (var i = 0; i < categoryBtns.length; i++){
+			categoryBtns[i].style.display = "inline-block";
+		}
+	var textInput = document.getElementById("todoInput").value;
+	var todoList = document.getElementById("todoList");
+	var newLine = document.createElement("li");
+	//here we add to our li a checkbox and a delete span with the id of the variable that keeps tracks on our lines
+	newLine.innerHTML = "<input class='check' type='checkbox' id='"+totalLines+"'><div class='note'>"+textInput+"</div> <span class=" +"delete"+ " onclick='delLine("+totalLines+");'>❌</span>";
   
-  //container.insertBefore(newLine, todoList);
-  todoList.appendChild(newLine);  
+  	todoList.appendChild(newLine);  
 }
 
-//this delete function gets called when we click the del span
+//this delete function gets called when we click the del span or
+//the button to remove all completed
 function delLine(lineID, delcompleted = 0) {
 
 	if(delcompleted){
@@ -105,13 +106,13 @@ function delLine(lineID, delcompleted = 0) {
 	}
 	
 	else if(lineID){
-			//it removes the parent of the element with the lineID
+		//removes the parent of the element with the lineID
 		document.getElementById(lineID).parentNode.remove();
 		//the variable gets updated
 		totalLines--;
 
 		//and if we dont have any todos we want to hide theese buttons again
-		if(totalLines<1) {
+		if(totalLines < 1) {
 			for (var i = 0; i < categoryBtns.length; i++){
 				categoryBtns[i].style.display = "none";
 			}
